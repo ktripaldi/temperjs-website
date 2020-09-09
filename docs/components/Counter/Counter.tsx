5dd34a792e4886f6c29064de494d62281806d9ff
Kenderson Tripaldi <kenderson.tripaldi@ors.ai>
2020-09-09 17:50:32 +0200

    Improve `Counter`

--- docs/components/Counter/Counter.tsx
+++ docs/components/Counter/Counter.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { useTrait, useTraitValue, SetterHelpers } from 'temperjs';
+import React from "react";
+import { useTrait, useTraitValue, SetterHelpers, WithSetter } from "temperjs";
 
 function Counter() {
-  const [count, setCount] = useTrait<number>('counter.count');
-  const isTargetReached = useTraitValue<boolean>('counter.isTargetReached');
+  const [count, setCount] = useTrait("counter.count") as WithSetter<number>;
+  const isTargetReached = useTraitValue("counter.isTargetReached") as boolean;
 
   function incrementCounter() {
-    setCount(({ value }: SetterHelpers<number>) => value as number + 1);
+    setCount(({ value }: SetterHelpers<number>) => (value as number) + 1);
   }
   function decrementCounter() {
-    setCount(({ value }: SetterHelpers<number>) => value as number - 1);
+    setCount(({ value }: SetterHelpers<number>) => (value as number) - 1);
   }
 
   return (
     <div>
-      <p>{count} { isTargetReached && (<span>You've reached the target!</span>)}</p>
+      <p>
+        {count} {isTargetReached && <span>You've reached the target!</span>}
+      </p>
       <button onClick={incrementCounter}>Increment</button>
       &nbsp;
       <button onClick={decrementCounter}>Decrement</button>
